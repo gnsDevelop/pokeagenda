091475c43dd0eaa97416b44bb14b9e8fa6f76dcf
Guilherme Nunes <gns.develop@gmail.com>
2024-03-20 22:21:50 -0300

    Melhorando card pokemon

--- src/app/_modules/_core/components/card-pokemon/card-pokemon.component.ts
+++ src/app/_modules/_core/components/card-pokemon/card-pokemon.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
     selector: 'card-pokemon',
@@ -6,4 +6,39 @@ import { Component } from "@angular/core";
     styleUrl: './card-pokemon.component.scss'
 })
 
-export class CardPokemonComponent {}
+export class CardPokemonComponent implements OnInit
+{
+    @Input() pokemonName: string;
+    @Input() pokemonNumber: number;
+    public image: string;
+    public imageNumber: string;
+
+    ngOnInit(): void 
+    {
+        this.buildImageUlr();
+    }
+
+    private buildImageUlr(): void
+    {
+        if (this.pokemonNumber <= 9)
+        {
+            this.imageNumber = '00' + this.pokemonNumber.toString();
+        }
+        else if (this.pokemonNumber > 9 && this.pokemonNumber <= 99)
+        {
+            this.imageNumber = '0' + this.pokemonNumber.toString();
+        }
+        else 
+        {
+            this.imageNumber = this.pokemonNumber.toString();
+        }
+
+        // Atribuir a variavel 'image' uma URL + o imageNumber
+        this.image = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/' + this.imageNumber + '.png';
+    }
+}
+
+// Ciclo de vida de um componente.
+// Nasce - Primeiro passo
+// Cresce - Mudanças
+// Morre - Destruir
---
+++ src/environments/environments.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    assetsPokemon: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png'
+};
